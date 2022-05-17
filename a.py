bf280fc6f9299852e72e9722a3a3a4275c68b865
rivery <rivery.wang@vesoft.com>
2022-05-17 10:58:31 +0800

    update

--- a.py
+++ a.py
@@ -1,4 +1,5 @@
 import functools
+import re
 
 
 def sort_cmp(number1, number2):
@@ -15,8 +16,17 @@ def sort_cmp(number1, number2):
     return 0 
 
 if __name__ == "__main__":
-    pr_sorted_list = [13, 1, 5, 7, 4, 19]
-    pr_list = [1, 5, 7, 13, 19, 8]
-    print(sorted(pr_list, key=functools.cmp_to_key(sort_cmp)))
+    # label_regex = '^v[0-9]*\.[0-9]*(.[0-9])?-cherry-pick$'
+    # prLabelRegex = re.compile(label_regex)
+    # label = "v2.2-cherry-pick"
+    # print(prLabelRegex.match(label))
+    
+    version_label_re = re.compile(r"^v[0-9]*\.[0-9]*(.[0-9])?")
+    version_label = "v2.2.0"  
+    print(version_label_re.match(version_label))
+    
+    # pr_sorted_list = [13, 1, 5, 7, 4, 19]
+    # pr_list = [1, 5, 7, 13, 19, 8]
+    # print(sorted(pr_list, key=functools.cmp_to_key(sort_cmp)))
     # print(len(list(filter(lambda x: x == '10', arr))))
 
