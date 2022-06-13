59ed949bc2adde84511ddd41f5e7d94b745ab9d1
Sophie <84560950+Sophie-Xie@users.noreply.github.com>
2022-06-13 15:43:01 +0800

    Add comment into pr if conflict, add ding notice

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ import time
 
 from pathlib import Path
 from github import Github
-# from dingtalkchatbot.chatbot import DingtalkChatbot
+from dingtalkchatbot.chatbot import DingtalkChatbot
 from sh import git
 from datetime import datetime
 
 
-# dingtalk_access_token = os.environ["INPUT_DINGTALK_ACCESS_TOKEN"]
-# dingtalk_secret = os.environ["INPUT_DINGTALK_SECRET"]
-# enable_dingtalk_notification = len(dingtalk_access_token) > 0 and len(dingtalk_secret) > 0
-# dingtalk_bot = DingtalkChatbot(
-#     webhook=f"https://oapi.dingtalk.com/robot/send?access_token={dingtalk_access_token}",
-#     secret=dingtalk_secret,
-# )
+dingtalk_access_token = os.environ["INPUT_DINGTALK_ACCESS_TOKEN"]
+dingtalk_secret = os.environ["INPUT_DINGTALK_SECRET"]
+enable_dingtalk_notification = len(dingtalk_access_token) > 0 and len(dingtalk_secret) > 0
+dingtalk_bot = DingtalkChatbot(
+     webhook=f"https://oapi.dingtalk.com/robot/send?access_token={dingtalk_access_token}",
+     secret=dingtalk_secret,
+)
 
 gh_url = "https://github.com"
 
@@ -191,7 +191,7 @@ def append_cherry_pick_in_msg(repo, pr):
     return "{}\nCherry-pick from {}\n\n".format(body, pr_link(repo, pr))
 
 
-def notify_author_by_comment(ent_repo, comm_repo, comm_ci, issue_num, comm_pr_num, org_members, conflict_files):
+def notify_author_by_comment(repo, comm_ci, issue_num, comm_pr_num, org_members, conflict_files):
     comment = ""
     if comm_ci.login() in org_members:
         comment += f"@{comm_ci.login()}\n"
@@ -222,9 +222,9 @@ CONFLICT FILES:
 ```
 """
 
-    issue = ent_repo.get_issue(issue_num)
-    issue.create_comment(comment.format(ent_repo.full_name,
-                                        ent_repo.name,
+    issue = repo.get_issue(issue_num)
+    issue.create_comment(comment.format(repo.full_name,
+                                        repo.name,
                                         issue_num,
                                         comm_pr_num,
                                         comm_pr_num,
@@ -332,6 +332,15 @@ def get_need_sync_prs(repo):
 #             commits.append(commit)
 #     return commits
 
+def get_org_name(repo):
+    l = repo.split('/')
+    assert len(l) == 2
+    return l[0]
+
+def get_org_members(org_name):
+    print(">>> Get org members")
+    org = gh.get_organization(org_name)
+    return [m.login for m in org.get_members()]
 
 def getFullVersion(label):
     if label.startswith('cherry-pick-'):
@@ -357,6 +366,7 @@ def getBaseBranch(repo, label):
 
 def generate_pr(repo, pr, label, commit_ci):
     try:
+        org_members = get_org_members(get_org_name(repo))
         baseBranch = getBaseBranch(repo, label)
         branch = "auto-pick-{}-to-{}".format(pr.number, baseBranch)
         new_pr_title = "[auto-pick-to-{}]{}".format(baseBranch, pr.title)
@@ -368,10 +378,20 @@ def generate_pr(repo, pr, label, commit_ci):
         time.sleep(2)
         new_pr = repo.get_pull(new_pr.number)
         new_pr.add_to_labels('auto-pick-robot')
-        if stopped:
+        
+        if stopped:            
+            notify_author_by_comment(repo,
+                baseBranch,
+                commit_ci,
+                new_pr.number,
+                commit_ci.pr_num,
+                org_members,
+                conflict_files)
             return (False, new_pr)
+        
         if not new_pr.mergeable:
             return (False, new_pr)
+        
         if should_auto_merge == 'true':
             commit_title = "{} (#{})".format(
                 commit_ci.title, new_pr.number)
@@ -382,6 +402,7 @@ def generate_pr(repo, pr, label, commit_ci):
         pr.add_to_labels(
             '{}-{}'.format(already_auto_pick_prefix, getFullVersion(label)))
         return (True, new_pr)
+    
     except Exception as e:
         print(">>> Fail to merge PR {}, cause: {}".format(pr.number, e))
 
@@ -396,10 +417,12 @@ def cherryPickByPrNum(repo, pr_num):
 def cherryPickPr(cur_repo, need_sync_prs):
     succ_pr_list = []
     err_pr_list = []
+    flag = 0
+    
     for (pr, commit_ci) in need_sync_prs:
         print("<<< head: {}, {}".format(pr.head.repo, pr.head.ref))
         labels = get_cherry_pick_pr_labels(pr)
-        print("<<< labels1111: {}".format(labels))
+        print("<<< labels: {}".format(labels))
         for label in labels:
             res = generate_pr(cur_repo, pr, label, commit_ci)
             md = pr_link(cur_repo, pr)
@@ -412,10 +435,20 @@ def cherryPickPr(cur_repo, need_sync_prs):
                         f">>> {pr_ref(cur_repo, res[1])} has been migrated from {pr_ref(cur_repo, pr)}")
                 else:
                     err_pr_list.append(md)
+                    flag = 1
                     print(
                         f">>> {pr_ref(cur_repo, pr)} could not be merged into {pr_ref(cur_repo, res[1])}")
                     break
-        break
+        if flag == 1:
+            break
+            
+    succ_prs = '\n\n'.join(succ_pr_list) if succ_pr_list else "None"
+    err_prs = '\n\n'.join(err_pr_list) if err_pr_list else "None"
+
+    print(">>> Enable dingtalk notification: {}".format(enable_dingtalk_notification))
+    if enable_dingtalk_notification and (len(succ_pr_list) > 0 or len(err_pr_list) > 0):
+        text = f"### Auto Cherry Pick Status\nPick successfully:\n\n{succ_prs}\n\nFailed to pick:\n\n{err_prs}"
+        dingtalk_bot.send_markdown(title='Auto Cherry Pick Status', text=text, is_at_all=False)
         
     print(">>> {} PRs need to sync, created {}, failed {}".format(
         len(need_sync_prs), len(succ_pr_list), len(err_pr_list)))
@@ -434,7 +467,6 @@ if __name__ == "__main__":
     print(">>> From: {}".format(cur_repo))
     add_repo_upstream(repo)
     generate_latest_100_commits(repo)
-    # print(">>> pr_num, {}".format(pr_num))
     if pr_num:
         cherryPickByPrNum(repo, pr_num)
     else:
