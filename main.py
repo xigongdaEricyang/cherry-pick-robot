a5ef653e58418d0b03b6f2504c1f54a4ed0c3e48
Sophie <84560950+Sophie-Xie@users.noreply.github.com>
2022-09-02 12:02:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ import time
 
 from pathlib import Path
 from github import Github
-from dingtalkchatbot.chatbot import DingtalkChatbot
+# from dingtalkchatbot.chatbot import DingtalkChatbot
 from sh import git
 from datetime import datetime
 
 
-dingtalk_access_token = os.environ["INPUT_DINGTALK_ACCESS_TOKEN"]
-dingtalk_secret = os.environ["INPUT_DINGTALK_SECRET"]
-enable_dingtalk_notification = len(dingtalk_access_token) > 0 and len(dingtalk_secret) > 0
-dingtalk_bot = DingtalkChatbot(
-     webhook=f"https://oapi.dingtalk.com/robot/send?access_token={dingtalk_access_token}",
-     secret=dingtalk_secret,
-)
+# dingtalk_access_token = os.environ["INPUT_DINGTALK_ACCESS_TOKEN"]
+# dingtalk_secret = os.environ["INPUT_DINGTALK_SECRET"]
+# enable_dingtalk_notification = len(dingtalk_access_token) > 0 and len(dingtalk_secret) > 0
+# dingtalk_bot = DingtalkChatbot(
+#     webhook=f"https://oapi.dingtalk.com/robot/send?access_token={dingtalk_access_token}",
+#     secret=dingtalk_secret,
+# )
 
 gh_url = "https://github.com"
 
@@ -72,6 +72,16 @@ class Commit:
             msg = title_re.match(commit.commit.message)
             if msg:
                 self.title = msg.group(1).strip()
+
+def get_org_members(org_name):
+    print(">>> Get org members")
+    org = gh.get_organization(org_name)
+    return [m.login for m in org.get_members()]
+
+def get_org_name(repo):
+    l = repo.split('/')
+    assert len(l) == 2
+    return l[0]
 
 
 def commit_changes(ci: Commit):
@@ -191,7 +201,7 @@ def append_cherry_pick_in_msg(repo, pr):
     return "{}\nCherry-pick from {}\n\n".format(body, pr_link(repo, pr))
 
 
-def notify_author_by_comment(repo, comm_ci, issue_num, comm_pr_num, org_members, conflict_files):
+def notify_author_by_comment(repo, comm_ci, sourcepr_num, branch, org_members, conflict_files):
     comment = ""
     if comm_ci.login() in org_members:
         comment += f"@{comm_ci.login()}\n"
@@ -210,10 +220,10 @@ You can use following commands to resolve the conflicts locally:
 $ git clone git@github.com:{}.git
 $ cd {}
 $ git remote -vv
-$ git fetch origin pull/{}/head:pr-{}
-$ git checkout pr-{}
+$ git fetch origin pull/{}/head:{}
+$ git checkout {}
 # resolve the conflicts
-$ git push -f origin pr-{}
+$ git push -f origin {}
 ```
 
 CONFLICT FILES:
@@ -222,13 +232,12 @@ CONFLICT FILES:
 ```
 """
 
-    issue = repo.get_issue(issue_num)
+    issue = repo.get_issue(sourcepr_num)
     issue.create_comment(comment.format(repo.full_name,
                                         repo.name,
-                                        issue_num,
-                                        comm_pr_num,
-                                        comm_pr_num,
-                                        comm_pr_num,
+                                        sourcepr_num, branch,
+                                        branch,
+                                        branch,
                                         '\n'.join(conflict_files)))
 
 
@@ -332,15 +341,6 @@ def get_need_sync_prs(repo):
 #             commits.append(commit)
 #     return commits
 
-def get_org_name(repo):
-    l = repo.split('/')
-    assert len(l) == 2
-    return l[0]
-
-def get_org_members(org_name):
-    print(">>> Get org members")
-    org = gh.get_organization(org_name)
-    return [m.login for m in org.get_members()]
 
 def getFullVersion(label):
     if label.startswith('cherry-pick-'):
@@ -364,46 +364,48 @@ def getBaseBranch(repo, label):
             raise Exception('base branch not found, label: {}'.format(label))
 
 
-def generate_pr(repo, pr, label, commit_ci):
+def generate_pr(repo, sourcepr, label, source_commitid, org_members):
     try:
-        org_members = get_org_members(get_org_name(repo))
         baseBranch = getBaseBranch(repo, label)
-        branch = "auto-pick-{}-to-{}".format(pr.number, baseBranch)
-        new_pr_title = "[auto-pick-to-{}]{}".format(baseBranch, pr.title)
-        body = append_cherry_pick_in_msg(repo, pr)
-        stopped, conflict_files = apply_patch(pr, baseBranch, branch, commit_ci)
+        branch = "auto-pick-{}-to-{}".format(sourcepr.number, baseBranch)
+        new_pr_title = "[auto-pick-to-{}]{}".format(baseBranch, sourcepr.title)
+
+        body = append_cherry_pick_in_msg(repo, sourcepr)
+        stopped, conflict_files = apply_patch(sourcepr, baseBranch, branch, source_commitid)
+
         new_pr = repo.create_pull(
             title=new_pr_title, body=body, head=branch, base=baseBranch)
         print(f">>> Create PR: {pr_link(repo, new_pr)}")
         time.sleep(2)
         new_pr = repo.get_pull(new_pr.number)
         new_pr.add_to_labels('auto-pick-robot')
-        
-        if stopped:            
+
+        if stopped:
             notify_author_by_comment(repo,
-                commit_ci,
-                new_pr.number,
-                commit_ci.pr_num,
-                org_members,
-                conflict_files)
+                                     source_commitid,
+                                     sourcepr.number,
+                                     branch,
+                                     org_members,
+                                     conflict_files)
             return (False, new_pr)
-        
+
         if not new_pr.mergeable:
             return (False, new_pr)
-        
+
         if should_auto_merge == 'true':
             commit_title = "{} (#{})".format(
-                commit_ci.title, new_pr.number)
+                source_commitid.title, new_pr.number)
             status = new_pr.merge(merge_method='squash',
                                   commit_title=commit_title)
             if not status.merged:
                 return (False, new_pr)
-        pr.add_to_labels(
+        sourcepr.add_to_labels(
             '{}-{}'.format(already_auto_pick_prefix, getFullVersion(label)))
         return (True, new_pr)
-    
     except Exception as e:
-        print(">>> Fail to merge PR {}, cause: {}".format(pr.number, e))
+        print(">>> Fail to merge PR {}, cause: {}".format(sourcepr.number, e))
+        return (False, new_pr)
+
 
 
 def cherryPickByPrNum(repo, pr_num):
@@ -417,12 +419,16 @@ def cherryPickPr(cur_repo, need_sync_prs):
     succ_pr_list = []
     err_pr_list = []
     flag = 0 
+    org_members = get_org_members(get_org_name(cur_repo))
+
     for (pr, commit_ci) in need_sync_prs:
+
         print("<<< head: {}, {}".format(pr.head.repo, pr.head.ref))
         labels = get_cherry_pick_pr_labels(pr)
-        print("<<< labels: {}".format(labels))
+        print("<<< labels1111: {}".format(labels))
+
         for label in labels:
-            res = generate_pr(cur_repo, pr, label, commit_ci)
+            res = generate_pr(cur_repo, pr, label, commit_ci,org_members)
             md = pr_link(cur_repo, pr)
             if res is not None:
                 if res[1].number >= 0:
@@ -457,6 +463,7 @@ if __name__ == "__main__":
     print(">>> From: {}".format(cur_repo))
     add_repo_upstream(repo)
     generate_latest_100_commits(repo)
+    # print(">>> pr_num, {}".format(pr_num))
     if pr_num:
         cherryPickByPrNum(repo, pr_num)
     else:
